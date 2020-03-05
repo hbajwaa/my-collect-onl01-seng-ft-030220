2e6a5fed788b56fc7ed42d83ba9ede62e7790490
Hassan Bajwa <bajwahassan3@outlook.com>
2020-03-05 07:32:08 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,18 @@
+def my_collect(array)
+  i = 0
+  new_collection = []
+  
+  while i < array.length
+    new_collection << yield(array[i])
+    i += 1
+  end
+  
+  return new_collection
+end
 
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 
+my_collect(array) do |name|
+  first_name = name.split(" ").first
+  first_name
+end
