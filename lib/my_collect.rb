956b8b39f3c86f5ddfffba9687723fac4ccf9342
Hassan Bajwa <bajwahassan3@outlook.com>
2020-03-05 07:23:50 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
+def my_collect(array)
+  i = 0
+  new_collection = []
+  
+  while i < array.length
+    new_collection << yield array[i]
+    i++
+  end
+  
+  return new_collection
+end
 
-
+my_collect(array) do |name|
+  return name.split(" ").first
+end
